caaa33f3156172bc0f73629384293c3b6fb98670
miikka.valkeapaa <miikka1973@gmail.com>
2018-07-15 13:26:29 +0300

    adds quotes in paths containing variables

--- manifests/dirs.pp
+++ manifests/dirs.pp
@@ -1,22 +1,22 @@
 class munki::dirs {
   $repositories = $munki::repositories
   $docroot      = $munki::docroot
-  
+
   File {
     owner	=> $munki::sambauser,
     group	=> $munki::sambauser,
     mode	=> '0777',
     recurse	=> true,
   }
-  
-  file { $docroot/munki: ensure => directory }
-  
+
+  file { "$docroot/munki": ensure => directory }
+
   $repositories.each |$locale| {
-    file { $docroot/munki/$locale: ensure => directory }
-    file { $docroot/munki/$locale/pkgs: ensure => directory }
-    file { $docroot/munki/$locale/pkgsinfo: ensure => directory }
-    file { $docroot/munki/$locale/manifests: ensure => directory }
-    file { $docroot/munki/$locale/catalogs: ensure => directory }
-    file { $docroot/munki/$locale/icons: ensure => directory }
+    file { "$docroot/munki/$locale": ensure => directory }
+    file { "$docroot/munki/$locale/pkgs": ensure => directory }
+    file { "$docroot/munki/$locale/pkgsinfo": ensure => directory }
+    file { "$docroot/munki/$locale/manifests": ensure => directory }
+    file { "$docroot/munki/$locale/catalogs": ensure => directory }
+    file { "$docroot/munki/$locale/icons": ensure => directory }
   }
 }
